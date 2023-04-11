b377a3fccc7910b56539cc5f13a6a05eb39e292d
deecodes222 <keysdeanna@gmail.com>
2023-04-11 15:04:01 -0400

    adds loops 3 and 4

--- Unit1/LoopsCon.js
+++ Unit1/LoopsCon.js
@@ -1,9 +1,26 @@
-// 1. Loops logs 0-99 Ascending
+//1. Loops logs 0-99 Ascending
 for (let i = 0; i<100; i++){
     console.log(i)
 }
 
+console.log("Number Two")
 //2. Loop log 99-0 Descending
 for (let i = 99; i>=0; i--){
     console.log(i)
+}
+
+console.log("Number Three")
+//3. Loop logs EVEN numbers 0-98 Ascending
+for(i=0; i<= 98; i++){
+    if(i % 2 ==0 ){
+        console.log(i)
+    }
+}
+
+console.log("Number 4")
+//4. Loops logs EVEN number from 98-0 Descending
+for(i=98; i>= 0; i--){
+    if(i%2==0){
+        console.log(i)
+    }
 }
